d94b14ddfa0de4e1c64e23cdf8f7474f18508236
Conner McCall <conner@clockwork.com>
2019-06-22 10:53:26 -0500

    add progress for goals

--- src/components/subCategories/index.js
+++ src/components/subCategories/index.js
@@ -1,14 +1,16 @@
 import React from "react"
 import Currency from '../currency';
 import { Table } from 'semantic-ui-react'
-
+import { Progress } from 'semantic-ui-react'
 function SubCategories({group}) {
+  let goals = group.reduce( (value, category) => {return value || category.goal_type === 'TBD'}, false);
   return <Table celled padded>
   	<Table.Header>
 		<Table.Row>
 	      <Table.HeaderCell singleLine>Category Name</Table.HeaderCell>
 	      <Table.HeaderCell>Balance</Table.HeaderCell>
 	      <Table.HeaderCell>Activity</Table.HeaderCell>
+        {goals && <Table.HeaderCell>Goal Progress</Table.HeaderCell>}
 	    </Table.Row>
   	</Table.Header>
   	<Table.Body>
@@ -20,6 +22,8 @@ function SubCategories({group}) {
       	 </Table.Cell>
       	 <Table.Cell><Currency cents={category.activity} />
       	 </Table.Cell>
+         {goals && <Table.Cell>{category.goal_type === 'TBD' && <Progress color='teal' percent={category.goal_percentage_complete} />}
+         </Table.Cell>}
   	 	</Table.Row>
       ))}
     </Table.Body>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,6 @@
 import React from "react"
 
 import Layout from "../components/layout"
-
-import BudgetName from "../components/budgetName";
 import Accounts from "../components/accounts";
 import Categories from "../components/categories";
 import { Divider } from 'semantic-ui-react'
